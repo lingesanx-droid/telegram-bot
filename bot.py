0ab9a4b10cb681b8029baaffb4a2c02c4b0b3d69
lingesanx-droid <lingesanx@gmail.com>
2026-05-11 14:31:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,66 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import asyncio
 
+TOKEN = "8719992437:AAEcApSXMMcXgCXY-E21IE4GaipKq3hG7L0"
+
+VIDEOS = []
+
+BUY_LINK = "https://t.me/unseenclipsbot"
+PROOF_LINK = "https://t.me/unseenxproof"
+ADMIN_LINK = "https://t.me/igmikasa"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    keyboard = [
+        [InlineKeyboardButton("💸 Buy Access", url=BUY_LINK)],
+        [InlineKeyboardButton("📸 Payment Proofs", url=PROOF_LINK)],
+        [InlineKeyboardButton("👑 Admin Contact", url=ADMIN_LINK)]
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    text = """
+🔥 FREE DEMO PREVIEW 🔥
+
+⏰ Demo auto delete in 2 minutes
+💎 Buy premium access below
+"""
+
+    await update.message.reply_text(
+        text,
+        reply_markup=reply_markup
+    )
+
+    sent_messages = []
+
+    for video in VIDEOS:
+        msg = await context.bot.send_video(
+            chat_id=update.effective_chat.id,
+            video=video
+        )
+        sent_messages.append(msg.message_id)
+
+    await asyncio.sleep(120)
+
+    for msg_id in sent_messages:
+        try:
+            await context.bot.delete_message(
+                chat_id=update.effective_chat.id,
+                message_id=msg_id
+            )
+        except:
+            pass
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="❌ Demo Expired\n\n💸 Buy Full Access Below",
+        reply_markup=reply_markup
+    )
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+print("Bot Running...")
+app.run_polling()
