47da9acc32c4d6a2d31ce87e6aba4a417fda15a4
lingesanx-droid <lingesanx@gmail.com>
2026-05-11 17:34:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,8 @@ import asyncio
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+# ================= WEB SERVER =================
+
 app_web = Flask(__name__)
 
 @app_web.route('/')
@@ -20,19 +22,27 @@ def run():
 
 Thread(target=run).start()
 
+# ================= BOT TOKEN =================
+
 TOKEN = "8719992437:AAF-Yz2YT2gpRhS7oN2CgO8i2ieVb2_Om9Y"
 
+# ================= VIDEOS =================
+
 VIDEOS = [
-    "BAACAgUAAxkBAAFJVglqAbv6GE6TgqlZF8B7XqSzAAGL9XAAAuAeAAL3LglUZ0eCl43OUeE7BA",
-    "BAACAgUAAxkBAAFJVgpqAbv6gA8WZmECWbeecEqcUxub2gAC4R4AAvcuCVReMdWdu4OrezsE",
-    "BAACAgUAAxkBAAFJVgtqAbv6PJp17U8ZmPtAjw9UBlxBKAAC4h4AAvcuCVQfgTGrVUhbNDsE",
+    "BAACAgUAAxkBAAFJVg1qAbv66nQ7N9ZuVrp5I7AC1IrT-QAC5B4AAvcuCVRu2LAGvDq2yTsE",
     "BAACAgUAAxkBAAFJVgxqAbv6vKrNs-YeC9i8omsWn8qFwgAC4x4AAvcuCVSrpuF_8WTpKjsE",
-    "BAACAgUAAxkBAAFJVg1qAbv66nQ7N9ZuVrp5I7AC1IrT-QAC5B4AAvcuCVRu2LAGvDq2yTsE"
+    "BAACAgUAAxkBAAFJVgtqAbv6PJp17U8ZmPtAjw9UBlxBKAAC4h4AAvcuCVQfgTGrVUhbNDsE",
+    "BAACAgUAAxkBAAFJVgpqAbv6gA8WZmECWbeecEqcUxub2gAC4R4AAvcuCVReMdWdu4OrezsE",
+    "BAACAgUAAxkBAAFJVglqAbv6GE6TgqlZF8B7XqSzAAGL9XAAAuAeAAL3LglUZ0eCl43OUeE7BA"
 ]
+
+# ================= BUTTON LINKS =================
 
 BUY_LINK = "https://t.me/unseenclipsbot"
 PROOF_LINK = "https://t.me/unseenxproofs"
 ADMIN_LINK = "https://t.me/igmikasa"
+
+# ================= START COMMAND =================
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
@@ -58,7 +68,9 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     sent_messages = []
 
+    # SEND VIDEOS
     for video in VIDEOS:
+
         msg = await context.bot.send_video(
             chat_id=update.effective_chat.id,
             video=video
@@ -66,8 +78,10 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
         sent_messages.append(msg.message_id)
 
+    # WAIT 2 MINUTES
     await asyncio.sleep(120)
 
+    # DELETE VIDEOS
     for msg_id in sent_messages:
         try:
             await context.bot.delete_message(
@@ -77,15 +91,19 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         except:
             pass
 
+    # EXPIRED MESSAGE
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
         text="❌ Demo Expired\n\n💸 Buy Full Access Below",
         reply_markup=reply_markup
     )
 
+# ================= RUN BOT =================
+
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
 
 print("Bot Running...")
+
 app.run_polling()
